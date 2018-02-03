2ad9c069a36efaf13012df33eb824f4d14b75b49
peerster <paer.berge@gmail.com>
2018-02-03 10:49:35 +0100

    Initial commit

---
+++ transfermission/cli.py
@@ -0,0 +1,103 @@
+#!/bin/env python
+import logging
+import re
+import click
+import transmissionrpc
+
+from utils import read_config, age, remove_torrent, process_item
+
+log = logging.getLogger()
+handler = logging.StreamHandler()
+formatter = logging.Formatter(
+        '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+handler.setFormatter(formatter)
+log.addHandler(handler)
+
+
+@click.command()
+@click.option(
+    '--dry-run',
+    help='Show what would be done',
+    is_flag=True,
+)
+@click.option(
+    '--log-level',
+    help='Log level',
+    default='info',
+)
+def cli(dry_run, log_level):
+    log_levels = {
+        'info': logging.INFO,
+        'debug': logging.DEBUG,
+    }
+    log.setLevel(log_levels.get(log_level))
+
+    config = read_config('transfermission_config.yaml')
+    movie_identifiers = config['movie_identifiers']
+    user = config['transmission_user']
+    password = config['transmission_password']
+    host = config['transmission_host']
+    port = config['transmission_port']
+
+    if dry_run:
+        log.info('Dry run mode. No changes will be done')
+
+    transmission_session = transmissionrpc.Client(address=host, port=port, user=user, password=password)
+
+    torrents = transmission_session.get_torrents()
+
+    for torrent in torrents:
+        operation = None
+        log.debug('Checking torrent %s', torrent.name)
+        log.debug('Status is: %s',  torrent.status)
+
+
+        # Use regex to identify if movie or tv serie.
+        # TODO: Use external resource (imdb etc) for this instead
+        if re.search('s\d\de\d\d', torrent.name, re.I):
+            file_type = 'series'
+
+        elif re.search('\.s\d\d\.', torrent.name, re.I):
+            log.info('Probably a whole season. Not supported...')
+            continue
+
+        elif True in (item.lower() in torrent.name for item in movie_identifiers):
+            file_type = 'movie'
+
+        else:
+            log.warning('Unable to identify file type of file %s', torrent.name)
+            log.debug('Current movie identifiers: %s' % movie_identifiers)
+
+        log.debug('File type is: %s', file_type)
+        file_seed_time = config.get('{0}_seed_time'.format(file_type))
+        log.debug('File max seed time in days: %s', file_seed_time)
+
+        torrent_age = 0
+        if torrent.date_done:
+            torrent_age = age(torrent.date_done)
+            log.debug('Torrent been seeding for %s day(s)', torrent_age)
+
+        if torrent_age > file_seed_time or torrent.isFinished:
+            log.debug('Torrent %s is complete', torrent.name)
+            operation = 'move'
+        elif torrent.status != 'downloading':
+            operation = 'symlink'
+
+        else:
+            log.warning('Torrent %s is not seeding or finished', torrent.name)
+
+        if operation == 'move':
+            result = remove_torrent(torrent, session=transmission_session, dry_run=dry_run)
+            if not result:
+                log.warning('Skipping moving torrent since stop failed')
+
+        if operation:
+            log.debug('Operation is: %s', operation)
+            process_item(item_type=file_type, torrent=torrent,
+                         operation=operation, config=config, dry_run=dry_run)
+        else:
+            log.error('No operation selected for torrent %s', torrent.name)
+
+
+if __name__ == '__main__':
+    cli()
---
+++ transfermission/utils.py
@@ -0,0 +1,153 @@
+import datetime
+import os
+import re
+import shutil
+from time import sleep
+import yaml
+import logging
+
+
+log = logging.getLogger(__name__)
+
+def read_config(file_path):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    config_file = "{0}/{1}".format(dir_path, file_path)
+    with open(config_file) as f:
+        config = yaml.load(f)
+
+    return config
+
+
+def age(date):
+    """
+    Return the number of days between now and date.
+    Date needs to be a datetime object.
+    """
+    time_difference = datetime.datetime.now() - date
+    return time_difference.days
+
+
+def remove_torrent(torrent, session, dry_run=False):
+    """
+    Stops and removes torrent from transmission
+    """
+    if dry_run:
+        log.info('Would stop and remove torrent %s', torrent.name)
+    else:
+        if torrent.status != 'stopped':
+            log.info('Stopping torrent %s', torrent.name)
+            session.stop_torrent(torrent.id)
+            for _ in range(5):
+                torrent_session = session.get_torrent(torrent.id)
+                log.debug(
+                    'torrent status is: %s', torrent_session.status
+                )
+                if torrent_session.status == 'stopped':
+                    break
+
+                sleep(3)
+
+            else:
+                log.warning('Failed to stop torrent %s', torrent.name)
+                return False
+
+        log.info('Removing torrent %s', torrent.name)
+        session.remove_torrent(torrent.id)
+        return True
+
+
+def move(file_src_path, file_dest_path, dry_run=False):
+    """ Move a file """
+    if dry_run:
+        log.info('Would move %s to %s' % (file_src_path, file_dest_path))
+    else:
+        log.info('Moving %s to %s' % (file_src_path, file_dest_path))
+        if os.path.islink(file_dest_path):
+            log.debug('destination is a link, removing it')
+            os.remove(file_dest_path)
+
+        shutil.move(file_src_path, file_dest_path)
+
+
+def handle_file(file_src_path, file_dest_path, operation, dry_run=False):
+    """ Symlink or move files """
+
+    if operation == 'symlink':
+        if not os.path.islink(file_dest_path):
+            if dry_run:
+                log.info("Would've Symlinked: %s to %s" % (file_dest_path, file_src_path))
+            else:
+                os.symlink(file_src_path, file_dest_path)
+                log.info('Created link: %s to %s' % (file_dest_path, file_src_path))
+        else:
+            log.debug('Symlink already exists')
+
+    elif operation == 'move':
+        move(file_src_path, file_dest_path, dry_run=dry_run)
+    else:
+        raise ValueError('Operation not supported')
+
+
+def process_item(item_type, torrent, config, operation, dry_run=False):
+    """ Process file and do various actions """
+
+    file_source = '{0}/{1}/'.format(
+        config.get('download_dir'),
+        torrent.name,
+    )
+
+    log.debug('File source path: %s', file_source)
+    file_target = None
+
+    if item_type == 'movie':
+        file_target = '{0}/{1}'.format(
+            config.get('movie_dir'),
+            torrent.name,
+        )
+
+    elif item_type == 'series':
+
+        season_regex = config.get('season_regex')
+        if not season_regex:
+            # I save my Season names as "Season 1", therefor exclude any starting zeros in season
+            season_regex = 's0?(\d+)e\d\d'
+        season = re.search(season_regex, torrent.name, re.I).group(1)
+        log.debug('Season: %s', season)
+
+        tv_show = None
+        for serie in os.listdir(config.get('series_dir')):
+            if serie.replace(" ", ".").lower() in torrent.name.lower():
+                tv_show = serie
+                break
+
+        if not tv_show:
+            log.warning('No matching tv show in %s for torrent %s' % (config.get('series_dir'), torrent.name))
+            return
+
+        if not season:
+            log.info('Unable to identify season')
+            return
+        else:
+            season_path = '{0}/{1}/Season {2}'.format(
+                config.get('series_dir'),
+                tv_show,
+                season,
+            )
+
+        if not os.path.isdir(season_path):
+            if not dry_run:
+                log.info('Missing season directory, creating it')
+                os.mkdir(season_path)
+            else:
+                log.info('Would create missing season dir: %s', season_path)
+
+        file_target = '{0}/{1}'.format(
+            season_path,
+            torrent.name,
+        )
+        log.debug('File destination: %s', file_target)
+
+    else:
+        log.debug("Item type '%s' unhandled", item_type)
+
+    handle_file(file_source, file_target, operation, dry_run)
